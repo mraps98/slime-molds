75b4629e0f3e29bcfd279085b8eddd22861cc714
Amritpreet Singh <amritpreet98@gmail.com>
2021-04-14 17:25:25 -0500

    new library initial test

--- js/app.js
+++ js/app.js
@@ -2,24 +2,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   /* variables */
   const canvas = document.querySelector("canvas#mainCanvas");
-  const ctx = canvas.getContext("2d");
-  ctx.translate(canvas.width / 3.5, canvas.height / 3);
-  ctx.rotate(Math.PI);
-
-  /* functions */
   const tree = new LSystem({
     axiom: "F++F++F",
     productions: {
       F: "F-F++F-F",
     },
-    finals: {
-      F: () => {
-        ctx.beginPath(), ctx.moveTo(0, 0);
-        ctx.lineTo(0, 25 / (tree.iterations + 1));
-        ctx.stoke(), ctx.translate(0, 25 / (tree.iterations + 1));
-      },
-    },
   });
 
   /* body */
+  const result = tree.iterate(2);
+  console.log(result);
 });
