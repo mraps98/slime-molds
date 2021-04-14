3b9bee0abdcaf8593bd4f3993da007f23df838b8
amritpreet98@gmail.com <amritpreet98@gmail.com>
2021-04-14 01:07:37 -0500

    using new library

--- js/app.js
+++ js/app.js
@@ -2,53 +2,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   /* variables */
   const canvas = document.querySelector("canvas#mainCanvas");
-  const t = new jjs.Turtle(canvas);
-  let ls;
+  const ctx = canvas.getContext("2d");
+  ctx.translate(canvas.width / 3.5, canvas.height / 3);
+  ctx.rotate(Math.PI);
 
   /* functions */
-  const kochCurve = (t) => {
-    var d0 = t.canvasWidth - 20;
-    var d;
-    var ls = new jjs.Lsystem(
-      {
-        F: function () {
-          t.forward(d);
-        },
-        "+": function () {
-          t.left(60);
-        },
-        "-": function () {
-          t.right(60);
-        },
+  const tree = new LSystem({
+    axiom: "F++F++F",
+    productions: {
+      F: "F-F++F-F",
+    },
+    finals: {
+      F: () => {
+        ctx.beginPath(), ctx.moveTo(0, 0);
+        ctx.lineTo(0, 25 / (tree.iterations + 1));
+        ctx.stoke(), ctx.translate(0, 25 / (tree.iterations + 1));
       },
-      [{ id: "F" }],
-      [
-        {
-          p: [{ id: "F" }],
-          s: function () {
-            return [
-              { id: "F" },
-              { id: "+" },
-              { id: "F" },
-              { id: "-" },
-              { id: "-" },
-              { id: "F" },
-              { id: "+" },
-              { id: "F" },
-            ];
-          },
-        },
-      ]
-    );
-    t.moveTo(-d0 / 2, -(t.canvasHeight / 2) + 10);
-    ls.beforeRender = function () {
-      d = d0 * (1 / Math.pow(3, ls.generation));
-    };
-    return ls;
-  };
+    },
+  });
 
   /* body */
-  ls = kochCurve(t);
-  ls.render();
-  console.log(ls.render);
 });
