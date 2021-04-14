4aa5d00d3df2124de4de21bfe7e64e461b7560a2
Amritpreet Singh <amritpreet98@gmail.com>
2021-04-14 17:49:02 -0500

    drawing koch snowflake

--- js/app.js
+++ js/app.js
@@ -2,14 +2,35 @@
 document.addEventListener("DOMContentLoaded", () => {
   /* variables */
   const canvas = document.querySelector("canvas#mainCanvas");
+  const ctx = canvas.getContext("2d");
+  ctx.translate(canvas.width / 2, canvas.height / 4);
+  const ANGLE_ROT = 60;
+  const finals = (s) => ({
+    "+": () => {
+      ctx.rotate((Math.PI / 180) * ANGLE_ROT);
+    },
+    "-": () => {
+      ctx.rotate((Math.PI / 180) * -60);
+    },
+    F: () => {
+      ctx.beginPath();
+      ctx.moveTo(0, 0);
+      ctx.lineTo(0, 40 / (s.iterations + 1));
+      ctx.stroke();
+      ctx.translate(0, 40 / (s.iterations + 1));
+    },
+  });
+
   const tree = new LSystem({
     axiom: "F++F++F",
     productions: {
       F: "F-F++F-F",
     },
   });
+  tree.setFinals(finals(tree));
 
   /* body */
   const result = tree.iterate(2);
   console.log(result);
+  tree.final();
 });
