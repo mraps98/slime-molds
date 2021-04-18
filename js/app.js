a4aaa4f4584b4e303e7867436e331430e060dd2b
Amritpreet Singh <amritpreet98@gmail.com>
2021-04-18 13:08:34 -0500

    slimemold axiom and production done

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
       ctx.rotate((Math.PI / 180) * ANGLE_ROT);
     },
     "-": () => {
-      ctx.rotate((Math.PI / 180) * -60);
+      ctx.rotate((Math.PI / 180) * -ANGLE_ROT);
     },
     F: () => {
       ctx.beginPath();
@@ -29,7 +29,52 @@ document.addEventListener("DOMContentLoaded", () => {
   });
   snowFlake.setFinals(finals(snowFlake));
 
+  const slimeMolds = new LSystem({
+    axiom: [
+      { symbol: "F", food: 1 },
+      { symbol: "+", food: 0 },
+      { symbol: "F", food: 1 },
+      { symbol: "+", food: 0 },
+      { symbol: "F", food: 1 },
+      { symbol: "+", food: 0 },
+      { symbol: "F", food: 1 },
+      { symbol: "+", food: 0 },
+      { symbol: "F", food: 1 },
+      { symbol: "+", food: 0 },
+      { symbol: "F", food: 1 },
+      { symbol: "+", food: 0 },
+    ],
+    productions: {
+      F: ({ part, index }) => {
+        if (part.food) {
+          return [
+            { symbol: "F", food: 1 },
+            { symbol: "-", food: 0 },
+            { symbol: "F", food: 1 },
+            { symbol: "F", food: 1 },
+            { symbol: "-", food: 0 },
+            { symbol: "-", food: 0 },
+            { symbol: "F", food: 1 },
+            { symbol: "F", food: 1 },
+            { symbol: "F", food: 1 },
+            { symbol: "F", food: 1 },
+            { symbol: "-", food: 0 },
+            { symbol: "-", food: 0 },
+          ];
+        } else {
+          return { symbol: "F", food: 0 };
+        }
+      },
+    },
+  });
+  slimeMolds.setFinals(finals(slimeMolds));
+
   /* body */
-  snowFlake.iterate(4);
-  snowFlake.final();
+  //snowFlake.iterate(4);
+  //snowFlake.final();
+
+  slimeMolds.iterate(7);
+  slimeMolds.final();
+
+  console.log(slimeMolds.getString());
 });
